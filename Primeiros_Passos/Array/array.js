c027cad1801a9c9566ee550310757f23e45afdf0
Lucas Saladini <lucassaladini@hotmail.com>
2023-12-30 12:38:06 -0300

    Working with loops and arrays

---
+++ Primeiros_Passos/Array/array.js
@@ -0,0 +1,32 @@
+const alunos = ['João', 'Vitor', 'Marina']
+
+console.log(alunos)
+console.log(alunos[0])
+
+// Adicionando 
+alunos.push('Renan')
+alunos[4] = 'Vinícius'
+console.log(alunos[3])
+console.log(alunos)
+
+// Substituindo
+alunos[3] = 'Amanda'
+console.log(alunos)
+
+// Lista vazia
+const  lista = []
+lista.push('teste')
+lista.push(10)
+
+console.log(lista)
+
+// Removendo
+const removendo = ['1', '2', '3']
+
+console.log(removendo)
+console.log(removendo.pop())
+console.log(removendo)
+
+console.log(removendo)
+console.log(removendo.shift())
+console.log(removendo)
---
+++ Primeiros_Passos/Array/for.js
@@ -0,0 +1,5 @@
+const nome = "Lucas Saladini"
+
+for (let i = 0; i < nome.length; i++) {
+    console.log(nome[i])
+}
